0ab7d608b4b4c5b8f1a4c2abba644c0293a813c8
asasa0903 <69354791+koki0903@users.noreply.github.com>
2023-06-21 20:46:12 +0900

    v1.2

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import subprocess
-
+from get_stock_prices import get_stock_prices
+from calculate_rs import calculate_rs
 
 print("株価を取得するプログラムを実行します。")
-
-# 株価を取得するプログラムを実行
-subprocess.run(["python", "get_stock_prices.py"])
+get_stock_prices()
 
 print("取得した株価からRS値を計算するプログラムを実行します。")
-
-# RSを計算するプログラムを実行
-subprocess.run(["python", "calculate_rs.py"])
+calculate_rs()
